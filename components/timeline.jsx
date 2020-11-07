cbe7f4313e66a83e07ee46d6f26361266fa8cdbf
Prabhat Pal <prabhat.pal@tantrumstreet.com>
2020-11-06 21:11:22 -0800

    added descitptions

--- components/timeline.jsx
+++ components/timeline.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import TimelineCard from "./timelineCard";
 
 const Timeline = () => {
@@ -7,29 +6,48 @@ const Timeline = () => {
       <h2>Timeline</h2>
       <div className="relative timeline max-w-4xl mx-auto mt-10">
         <TimelineCard
+          type="work"
           title="Zen3 Infosolution America Inc."
           subTitle="Build Engineer"
-          type="work"
+          descriptions={[
+            "Write script to automate various stages in the deployment process. Monitor and maintain the deployment process. - Build front-end reusable components for the product.",
+            "Build front-end reusable components for the product 'Assignments' (– a complete classroom management tool by Microsoft). Work on fixing bugs, writing efficient accessibility code and testing the application.",
+          ]}
         />
         <TimelineCard
+          type="work"
           title="Tantrum Street LLC"
           subTitle="Software Engineer"
-          type="work"
+          descriptions={[
+            "Developed independent and reusable React components using ReasonML for 'Autoboard' (– a merchant onboarding platform for ISOs).",
+            "Developed scalable micro-service APIs.",
+            "Refactored entire application Autoboard, to create a fast, responsive and highly scalable web application.",
+            "Worked on the mobile app for Autoboard using React Native.",
+          ]}
         />
         <TimelineCard
+          type="education"
           title="Southern Methodist University"
           subTitle="Master's in Computer Science"
-          type="education"
         />
         <TimelineCard
+          type="work"
           title="Siemens Healthineers"
           subTitle="Senior Systems Engineer"
-          type="work"
+          descriptions={[
+            "Implemented modules for a library which was being used by multiple projects process images scanned from a CT scanner machine.",
+            "Implemented automated test modules for multiple functionalities.",
+          ]}
         />
         <TimelineCard
+          type="work"
           title="Capgemini"
           subTitle="Senior Software Engineer"
-          type="work"
+          descriptions={[
+            "Build a web tool to compare databases and migrate data from one another, which helped the team to sync data with multiple environments.",
+            "Received ‘Best Innovative Thinking’ award for building a web tool to automate generation of more than 100 reports, thereby reducing the deliverable turn-around by 15%.",
+            "Designed and build a Project Tracing tool to manage different tasks of a project to make the process smooth.",
+          ]}
         />
       </div>
     </section>
--- components/timelineCard.jsx
+++ components/timelineCard.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 
-const TimelineCard = ({ title, subTitle, type }) => {
+const TimelineCard = ({ title, subTitle, descriptions = [], type }) => {
   return (
     <div className={`flex ${type !== "work" ? "md:flex-row-reverse" : ""}`}>
       <div
         className={`relative work w-4/5 md:w-2/5 mx-auto ${
           type === "work" ? "md:ml-0" : "md:mr-0"
-        } bg-primary-200 rounded-xl p-2 text-left mb-4`}
+        } bg-primary-200 rounded-xl p-4 text-left mb-4`}
       >
         {type === "education" && (
           <div className="absolute grad-cap">
@@ -14,7 +14,12 @@ const TimelineCard = ({ title, subTitle, type }) => {
           </div>
         )}
         <h3>{title}</h3>
-        <p>{subTitle}</p>
+        <h4>{subTitle}</h4>
+        <ul className="pt-4 list-disc pl-4">
+          {descriptions.map((description, i) => (
+            <li key={i}>{description}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
