7fc0e82849effc90519479dc9a3a72685bb82345
Prabhat Pal <prabhat.pal@tantrumstreet.com>
2020-11-07 23:20:10 -0800

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,25 @@ class MyDocument extends Document {
           <meta charSet="UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
 
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
           <link
             href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300;400;500;600;700&family=Quattrocento+Sans:wght@400;700&display=swap"
             rel="stylesheet"
