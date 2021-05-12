3844f524a843360c40b84c6665ed6636f1a50a2c
palprabhat <prabhatpal.14@gmail.com>
2021-05-12 14:58:30 -0700

    add test coverage to code-climate

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,13 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
   testMatch: ["**/__tests__/*.test.js?(x)"],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
+  modulePaths: ["<rootDir>"],
+  testURL: "http://localhost/",
+  collectCoverageFrom: [
+    "**[/components|/hooks|/pages/utils]?/*.{ts,tsx,js,jsx}",
+  ],
+  collectCoverage: false,
+  coverageDirectory: "test_reports/coverage",
 };
